131b30353beb772b68db30969eac3e035a5e4ba3
39sho <g.39sho@gmail.com>
2024-10-10 10:19:30 +0900

    /public/vite.svg -> /vite.svg

--- src/app/Document.tsx
+++ src/app/Document.tsx
@@ -4,7 +4,7 @@ export const Document = ({ children }: { children: ReactNode }) => (
 	<html lang="ja">
 		<head>
 			<meta charSet="UTF-8" />
-			<link rel="icon" type="image/svg+xml" href="/public/vite.svg" />
+			<link rel="icon" type="image/svg+xml" href="/vite.svg" />
 			<meta name="viewport" content="width=device-width, initial-scale=1" />
 			<title>vite-node + React</title>
 		</head>
